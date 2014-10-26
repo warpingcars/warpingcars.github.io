fe2fe5dd7abfd903d4e5f30c75d08dd66c95ea4e
warpingcars <oscarwarpe@gmail.com>
2014-10-26 21:01:37 +0100

    modified:   molly/index.html 	modified:   molly/script.js

--- molly/script.js
+++ molly/script.js
@@ -1,22 +1,37 @@
 $(document).ready(function()
 {
 	
-	$("#mollyimg").click(function(){
-		var mollyimg = ''; 
-	   $("#hidden").click(); 
-	   $("#show").val(mollyimg); 
+	$("#mollyimg").click(function()
+	{ 
+	if ($("#mollyimg").attr('src') == "")
+	{
+	    $("#hidden").click(); 
+	}
+	else 
+	{
+		alert("Do you want to MollyWang this image?"); 
+	}
+	   
 	}); 
 	
-	$("#show").click(function(){ 
-		var mollyimg = $("#hidden").val(); 
-	    $("#show").val(mollyimg); 
-	}); 
 }); 
 
-function change()
-{
-	
-	$("#show").val($("#hidden").val()); 
-	$("#mollyimg").val($("#hidden").val()); 
-
-}
+function change(fileInput) {
+        var files = fileInput.files;
+        for (var i = 0; i < files.length; i++) {           
+            var file = files[i];
+            var imageType = /image.*/;     
+            if (!file.type.match(imageType)) {
+                continue;
+            }           
+            var img=document.getElementById("mollyimg");            
+            img.file = file;    
+            var reader = new FileReader();
+            reader.onload = (function(aImg) { 
+                return function(e) { 
+                    aImg.src = e.target.result; 
+                }; 
+            })(img);
+            reader.readAsDataURL(file);
+        }    
+    }
