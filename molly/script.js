78128a0839bcaa47d9b63464fd2cf764778b7670
warpingcars <oscarwarpe@gmail.com>
2014-10-26 19:59:27 +0100

    modified:   molly/index.html 	modified:   molly/script.js 	modified:   molly/style.css

--- molly/script.js
+++ molly/script.js
@@ -2,12 +2,21 @@ $(document).ready(function()
 {
 	
 	$("#mollyimg").click(function(){
+		var mollyimg = ''; 
 	   $("#hidden").click(); 
+	   $("#show").val(mollyimg); 
 	}); 
 	
 	$("#show").click(function(){ 
 		var mollyimg = $("#hidden").val(); 
 	    $("#show").val(mollyimg); 
-	    $("#mollyimg").css('background', '"url('+mollyimg+')"'); 
 	}); 
 }); 
+
+function change()
+{
+	
+	$("#show").val($("#hidden").val()); 
+	$("#mollyimg").val($("#hidden").val()); 
+
+}
