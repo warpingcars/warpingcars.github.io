f63578d0d51c9c6f6ccea1e12e2bb4a74ec6f0d9
warpingcars <oscarwarpe@gmail.com>
2014-10-28 00:04:20 +0100

    modified:   molly/index.html 	modified:   molly/script.js 	modified:   molly/style.css

--- molly/script.js
+++ molly/script.js
@@ -1,5 +1,20 @@
 $(document).ready(function()
 { 
+    document.body.addEventListener('touchmove', function(e)
+        { e.preventDefault(); 
+        }); 
+    window.addEventListener("touchstart", function(event)
+    {
+             if(event.target.tagName=="HTML" || event.target.tagName=="BODY")
+             {
+                     event.preventDefault();
+             }
+    } ,false);
+    window.addEventListener("scroll",function()
+    {
+    window.scrollTo(0,0)
+    },false); 
+
 	$("#tap_pic").click(function()
     { 
         $("#mollyimg").click(); 
