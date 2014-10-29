f207fd310991c12c7dd0b06d5b6d0ca63a4fcd83
warpingcars <oscarwarpe@gmail.com>
2014-10-30 00:03:11 +0100

    modified:   molly2/index.html 	new file:   molly3/index.html 	new file:   molly3/script.js 	new file:   molly3/style.css

---
+++ molly3/script.js
@@ -0,0 +1,84 @@
+
+
+$(document).ready(function()
+{ 
+/*
+    document.body.addEventListener('touchmove', function(e)
+        { e.preventDefault(); 
+        }); 
+    window.addEventListener("touchstart", function(event)
+    {
+             if(event.target.tagName=="HTML" || event.target.tagName=="BODY")
+             {
+                     event.preventDefault();
+             }
+    } ,false); 
+    window.addEventListener("scroll",function()
+    {
+    window.scrollTo(0,0)
+    },false); 
+
+	$("#tap_pic").click(function()
+    { 
+        $("#mollyimg").click(); 
+
+    }); 
+*/
+
+	$("#mollyimg").click(function() 
+	{ 
+	if ($("#mollyimg").attr('src') == "")
+	{
+        $("#tap_pic").attr( "class", "hidden" ); 
+        $("#mollyimg").attr( "class", "imgedit" ); 
+        $("#toolbox_up").attr( "class", "toolbox" ); 
+        $("#toolbox_down").attr( "class", "toolbox" ); 
+        $("#hidden").click(); 
+	}
+	else  
+	{ 
+        alert("You tapped your MollyWang. That's nasty! "); 
+	}
+	   
+	}); 
+	
+    $("#toolbox_up").click(function() 
+    {
+        alert("We are currently out of Molly Accessories. Please try again next week! "); 
+    }); 
+    $("#toolbox_down").click(function() 
+    {
+        alert("Molly Effects need further research in order to achieve an enhanced MollyWang experience. Be patient! "); 
+    }); 
+
+
+    $("#exit").click(function() 
+    {
+        $("#mollyimg").attr('src', ""); 
+        $("#tap_pic").removeClass( "hidden" ); 
+        $("#mollyimg").attr( "class", "imgcap" ); 
+        $("#toolbox_up").attr( "class", "toolbox_disabled" ); 
+        $("#toolbox_down").attr( "class", "toolbox_disabled" ); 
+    }); 
+
+}); 
+
+function change(fileInput) {
+        var files = fileInput.files;
+        for (var i = 0; i < files.length; i++) {           
+            var file = files[i];
+            var imageType = /image.*/;     
+            if (!file.type.match(imageType)) {
+                continue;
+            }           
+            var img=document.getElementById("mollyimg");            
+            img.file = file;    
+            var reader = new FileReader();
+            reader.onload = (function(aImg) { 
+                return function(e) { 
+                    aImg.src = e.target.result; 
+                }; 
+            })(img);
+            reader.readAsDataURL(file);
+        }    
+    }
