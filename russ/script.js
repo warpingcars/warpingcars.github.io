4fca100031b999df8fe730d5b20a7da16ef37413
warpingcars <oscarwarpe@gmail.com>
2014-10-30 21:47:07 +0100

    new file:   extra/heart/poll_result.txt 	new file:   extra/heart/poll_vote.php 	modified:   russ/index.html 	modified:   russ/script.js

--- russ/script.js
+++ russ/script.js
@@ -102,4 +102,25 @@ function rotateRussekort()
     degrees = 0; 
     }
 
+} 
+
+function getVote(int)
+{
+    if (window.XMLHttpRequest)
+    {// code for IE7+, Firefox, Chrome, Opera, Safari
+        xmlhttp=new XMLHttpRequest();
+    }
+    else
+    {// code for IE6, IE5
+        xmlhttp=new ActiveXObject("Microsoft.XMLHTTP");
+    }
+    xmlhttp.onreadystatechange=function()
+    {
+        if (xmlhttp.readyState==4 && xmlhttp.status==200)
+        {
+            document.getElementById("poll").innerHTML=xmlhttp.responseText;
+        }
+    }
+    xmlhttp.open("GET","poll_vote.php?vote="+int,true);
+    xmlhttp.send();
 }
